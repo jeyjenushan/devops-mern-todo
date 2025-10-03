f8731fda9767a2b29a4a21c7658be089654a9ff4
Jeyaruban Jenushan <jjenushan550@gmail.com>
2025-10-03 07:04:09 +0530

    issue fixed

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log('MongoDB connected is'))
+}).then(() => console.log('MongoDB connected '))
   .catch(err => console.error(err));
 
 const tasksRoute = require('./routes/tasks');
